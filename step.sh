ed72cdab4b81840f5099d728b15ad2681bcd2490
Bo Han <bo.han@getintheloop.ca>
2016-06-08 17:02:08 -0700

    - Fixed install google api libs outside directory issue - Added bitrise.yml, now step can run through Bitrise CLI

--- step.sh
+++ step.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
-
 THIS_SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 DRIVE_API_LIB_PATH="${THIS_SCRIPTDIR}/libs/drive_api"
 
+if $BITRISE_CLI = 'true'; then
+  DRIVE_API_LIB_PATH=$PATH_TO_GOOGLE_API_INSTALLER
+fi
+
 echo "Installing Google Drive python Library..."
-python "${DRIVE_API_LIB_PATH}/setup.py" install --user
+(cd ${DRIVE_API_LIB_PATH} && python setup.py install --user)
+
 echo "Drive Library installed"
 
 echo "Running script..."
